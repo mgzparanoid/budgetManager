37f48870a4bdcf8da2b1a37979a678400e93f271
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2023-05-25 14:54:29 +0200

    Refactored HomeController.

--- src/main/java/com/mgzparanoid/budgetmanager/controllers/HomeController.java
+++ src/main/java/com/mgzparanoid/budgetmanager/controllers/HomeController.java
@@ -1,18 +1,13 @@
 package com.mgzparanoid.budgetmanager.controllers;
 
-//import com.mgzparanoid.budgetmanager.entities.FixedMonthlyEarnings;
-//import com.mgzparanoid.budgetmanager.entities.FixedMonthlySpending;
-//import com.mgzparanoid.budgetmanager.repositories.FixedMonthlyEarningsRepository;
-//import com.mgzparanoid.budgetmanager.repositories.FixedMonthlySpendingRepository;
 import com.mgzparanoid.budgetmanager.entities.FixedMonthlyIncomeAndExpenses;
 import com.mgzparanoid.budgetmanager.repositories.FixedMonthlyIncomeAndExpensesRepository;
-import com.mgzparanoid.budgetmanager.repositories.FundsRepository;
 import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -22,13 +17,11 @@ public class HomeController {
 
     @GetMapping("/spending")
     public List<FixedMonthlyIncomeAndExpenses> spending() {
-        List<FixedMonthlyIncomeAndExpenses> fundsSpendings = fixedMonthlyIncomeAndExpensesRepository.getAllExpenses().stream().collect(Collectors.toList());
-        return fundsSpendings;
+        return new ArrayList<>(fixedMonthlyIncomeAndExpensesRepository.getAllExpenses());
     }
     @GetMapping("/earnings")
     public List<FixedMonthlyIncomeAndExpenses> earnings() {
-        List<FixedMonthlyIncomeAndExpenses> fundsSpendings = fixedMonthlyIncomeAndExpensesRepository.getAllIncome().stream().collect(Collectors.toList());
-        return fundsSpendings;
+        return new ArrayList<>(fixedMonthlyIncomeAndExpensesRepository.getAllIncome());
     }
 
     @DeleteMapping("/fixedmontly/delete/{id}")
